e098aeac9dbd164b7c9ba1bd021a2b7a7082e271
Light808 <178133326+Light808@users.noreply.github.com>
2025-08-11 17:15:18 +0700

    Update UrlController.cs

--- UrlShortener/Controllers/UrlController.cs
+++ UrlShortener/Controllers/UrlController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using UrlShortener.Data;
 using UrlShortener.Models;
 using System;
+using System.Linq;
 
 namespace UrlShortener.Controllers
 {
@@ -16,6 +17,7 @@ namespace UrlShortener.Controllers
             _context = context;
         }
 
+        // POST: api/url/shorten
         [HttpPost("shorten")]
         public IActionResult Shorten([FromBody] string originalUrl)
         {
@@ -23,6 +25,13 @@ namespace UrlShortener.Controllers
                 return BadRequest("Invalid URL format.");
 
             var shortCode = Guid.NewGuid().ToString().Substring(0, 6);
+
+            // Check for collisions (rare, but safer)
+            while (_context.ShortUrls.Any(u => u.ShortCode == shortCode))
+            {
+                shortCode = Guid.NewGuid().ToString().Substring(0, 6);
+            }
+
             var shortUrl = new ShortUrl
             {
                 OriginalUrl = originalUrl,
@@ -34,6 +43,25 @@ namespace UrlShortener.Controllers
 
             var result = $"{Request.Scheme}://{Request.Host}/{shortCode}";
             return Ok(result);
+        }
+
+        [HttpGet("{shortCode}")]
+        public IActionResult GetOriginalUrl(string shortCode)
+        {
+            if (string.IsNullOrWhiteSpace(shortCode))
+                return BadRequest("Short code is required.");
+
+            var link = _context.ShortUrls.FirstOrDefault(u => u.ShortCode == shortCode);
+            if (link == null)
+                return NotFound("Short link not found.");
+
+            return Ok(new
+            {
+                OriginalUrl = link.OriginalUrl,
+                ShortCode = link.ShortCode,
+                ShortUrl = $"{Request.Scheme}://{Request.Host}/{link.ShortCode}",
+                ClickCount = link.ClickCount
+            });
         }
     }
 }
